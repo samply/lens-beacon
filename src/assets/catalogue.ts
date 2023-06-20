5fb60ed23fe6a8c0228b3b14f52fce5ec714e763
Croft <d506t@ad.dkfz-heidelberg.de>
2023-06-20 17:18:07 +0200

    Customized search tree for Beacon

--- src/assets/catalogue.ts
+++ src/assets/catalogue.ts
@@ -1,144 +1,212 @@
-import { Category, Criteria } from '@samply/lens-core';
-import { MolecularMarker } from './molecular-marker';
+import {Category, Criteria} from "@samply/lens-core";
 
 export const staticCatalogue: Array<Category> = [
-  new Category('patient', 'Patient', [
-    new Criteria(
-      'gender',
-      { de: 'Geschlecht', en: 'Sex' },
-      'string',
-      '',
-      ['IN'],
+  new Category(
+    "MolecularMarker1",
+    "Genetics",
+    [
+      new Criteria(
+        "genomic_variation",
+        // {de: "DNAänderung, z.B. 22:g.16050075A>G", en: "DNA modification, e.g. 22:g.16050075A>G"},
+        {de: "DNA modification, e.g. 22:g.16050075A>G", en: "DNA modification, e.g. 22:g.16050075A>G"},
+        "string",
+        "",
+        ["EQUALS"],
+      ),
+      new Criteria(
+        "ensembl_id",
+        // {de: "EnsemblID, z.B. ENSG00000125968", en: "EnsemblID, e.g. ENSG00000125968"},
+        {de: "EnsemblID, e.g. ENSG00000125968", en: "EnsemblID, e.g. ENSG00000125968"},
+        "string",
+        "",
+        ["EQUALS"],
+      ),]
+  ),
+  new Category(
+      "general_data",
+      "Clinical data",
       [
-        {
-          key: 'male',
-          de: 'männlich',
-          en: 'male',
-        },
-        {
-          key: 'female',
-          de: 'weiblich',
-          en: 'female',
-        },
-        {
-          key: 'other',
-          de: 'Sonstiges / intersexuell',
-          en: 'diverse',
-        },
-        {
-          key: 'unknown',
-          de: 'unbekannt',
-          en: 'unknown',
-        },
+        new Criteria(
+          "gender",
+          // {en: "Sex", de: "Geschlecht"},
+          {en: "Sex", de: "Sex"},
+          "string",
+          "",
+          ["IN"],
+          [
+            {
+              "key": "male",
+              "en": "male",
+              // "de": "männlich"
+              "de": "male"
+            },
+            {
+              "key": "female",
+              "en": "female",
+              // "de": "weiblich"
+              "de": "female"
+            },
+            {
+              "key": "other",
+              "en": "diverse",
+              // "de": "Sonstiges / intersexuell"
+              "de": "diverse"
+            },
+            {
+              "key": "unknown",
+              "en": "unknown",
+              // "de": "unbekannt"
+              "de": "unknown"
+            }
+          ]
+        ),
+        new Criteria(
+          "ethnicity",
+          // {en: "Sex", de: "Geschlecht"},
+          {en: "Ethnicity", de: "Ethnicity"},
+          "string",
+          "",
+          ["IN"],
+          [
+            {
+              "key": "African",
+              "en": "African",
+              "de": "African"
+            },
+            {
+              "key": "British",
+              "en": "British",
+              "de": "British"
+            },
+            {
+              "key": "Asian or Asian British",
+              "en": "Asian or Asian British",
+              "de": "Asian or Asian British"
+            },
+            {
+              "key": "White and Asian",
+              "en": "White and Asian",
+              "de": "White and Asian"
+            },
+            {
+              "key": "White and Black African",
+              "en": "White and Black African",
+              "de": "White and Black African"
+            },
+            {
+              "key": "Any other Asian background",
+              "en": "Any other Asian background",
+              "de": "Any other Asian background"
+            },
+            {
+              "key": "White and Black Caribbean",
+              "en": "White and Black Caribbean",
+              "de": "White and Black Caribbean"
+            },
+            {
+              "key": "Black or Black British",
+              "en": "Black or Black British",
+              "de": "Black or Black British"
+            },
+            {
+              "key": "Any other mixed background",
+              "en": "Any other mixed background",
+              "de": "Any other mixed background"
+            },
+            {
+              "key": "Bangladeshi",
+              "en": "Bangladeshi",
+              "de": "Bangladeshi"
+            },
+            {
+              "key": "Any other Black background",
+              "en": "Any other Black background",
+              "de": "Any other Black background"
+            },
+            {
+              "key": "Irish",
+              "en": "Irish",
+              "de": "Irish"
+            },
+            {
+              "key": "Mixed",
+              "en": "Mixed",
+              "de": "Mixed"
+            },
+            {
+              "key": "White",
+              "en": "White",
+              "de": "White"
+            },
+            {
+              "key": "Pakistani",
+              "en": "Pakistani",
+              "de": "Pakistani"
+            },
+            {
+              "key": "Caribbean",
+              "en": "Caribbean",
+              "de": "Caribbean"
+            },
+            {
+              "key": "Other ethnic group",
+              "en": "Other ethnic group",
+              "de": "Other ethnic group"
+            },
+            {
+              "key": "Indian",
+              "en": "Indian",
+              "de": "Indian"
+            },
+            {
+              "key": "Any other white background",
+              "en": "Any other white background",
+              "de": "Any other white background"
+            },
+            {
+              "key": "Chinese",
+              "en": "Chinese",
+              "de": "Chinese"
+            }
+          ]
+        )
+      ]
+    ),
+    new Category(
+      "biosamples",
+      "Biosample",
+      [
+        new Criteria(
+          "sample_kind",
+          // {de: "Probenart", en: "Sample kind"},
+          {de: "Sample type", en: "Sample type"},
+          "string",
+          "",
+          ["IN"],
+          [
+            {
+              "key": "blood",
+              // "de": "Vollblut",
+              "de": "Whole blood",
+              "en": "Whole blood"
+            },
+            {
+              "key": "blood-serum",
+              "de": "Serum",
+              "en": "Serum"
+            },
+            {
+              "key": "blood-plasma",
+              "de": "Plasma",
+              "en": "Plasma"
+            },
+            {
+              "key": "lymph",
+              "de": "Lymph",
+              "en": "Lymph"
+            }
+          ]
+        )
       ]
     )
-    ]
-  ),
-  new Category('biosamples', 'Bioproben', [
-    new Criteria( // TODO must be adapted to the bbmri valueSet
-      'sample_kind',
-      { de: 'Probenart', en: 'Sample kind' },
-      'string',
-      '',
-      ['IN'],
-      [
-        {
-          key: 'tumor-tissue-ffpe',
-          de: 'Tumorgewebe (FFPE)',
-          en: 'Tumor tissue (FFPE)',
-        },
-        {
-          key: 'tumor-tissue-frozen',
-          de: 'Tumorgewebe (Kryo/Frisch)',
-          en: 'Tumor tissue (frozen)',
-        },
-        {
-          key: 'normal-tissue-ffpe',
-          de: 'Normalgewebe (FFPE)',
-          en: 'Normal tissue (FFPE)',
-        },
-        {
-          key: 'normal-tissue-frozen',
-          de: 'Normalgewebe (Kryo/Frisch)',
-          en: 'Normal tissue (frozen)',
-        },
-        {
-          key: 'whole-blood',
-          de: 'Vollblut',
-          en: 'Whole blood',
-        },
-        {
-          key: 'blood-serum',
-          de: 'Serum',
-          en: 'serum',
-        },
-        {
-          key: 'blood-plasma',
-          de: 'Plasma',
-          en: 'Plasma',
-        },
-        {
-          key: 'urine',
-          de: 'Urin',
-          en: 'urine',
-        },
-        {
-          key: 'csf-liquor',
-          de: 'Liquor/CSF',
-          en: 'CSF/Liquor',
-        },
-        {
-          key: 'stool-faeces',
-          de: 'Stuhl/Kot',
-          en: 'Stool/Faeces',
-        },
-        {
-          key: 'bone-marrow',
-          de: 'Knochenmark',
-          en: 'Bone marrow',
-        },
-        {
-          key: 'dna',
-          de: 'DNA',
-          en: 'DNA',
-        },
-        {
-          key: 'rna',
-          de: 'RNA',
-          en: 'RNA',
-        }
-      ]
-    ),
-  ]),
-  new Category('MolecularMarker1', 'Molekulare Marker', [
-    MolecularMarker,
-    new Criteria(
-      '48005-3',
-      { de: 'Aminosäureänderung', en: 'Aminoacidchange' },
-      'string',
-      'http://loinc.org',
-      ['EQUALS']
-    ),
-    new Criteria(
-      '81290-9',
-      { de: 'DNA Änderung', en: 'DNAchange' },
-      'string',
-      'http://loinc.org',
-      ['EQUALS']
-    ),
-    new Criteria(
-      '81248-7',
-      { de: 'SeqRefNCBI', en: 'SeqRefNCBI' },
-      'string',
-      'http://loinc.org',
-      ['EQUALS']
-    ),
-    new Criteria(
-      '81249-5',
-      { de: 'EnsemblID', en: 'EnsemblID' },
-      'string',
-      'http://loinc.org',
-      ['EQUALS']
-    ),
-  ]),
-];
+]
